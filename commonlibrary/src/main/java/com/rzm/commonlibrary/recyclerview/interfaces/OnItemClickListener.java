42cadb69fb15b53c068fc5ceddbaa18969a11215
renzhenming <1565766177@qq.com>
2017-07-22 18:55:58 +0800

    add recyclerview

---
+++ commonlibrary/src/main/java/com/rzm/commonlibrary/recyclerview/interfaces/OnItemClickListener.java
@@ -0,0 +1,10 @@
+package com.rzm.commonlibrary.recyclerview.interfaces;
+
+import android.view.View;
+
+/**
+ * Click
+ */
+public interface OnItemClickListener {
+    void onItemClick(View view, int position);
+}
---
+++ commonlibrary/src/main/java/com/rzm/commonlibrary/recyclerview/interfaces/OnItemLongClickListener.java
@@ -0,0 +1,11 @@
+package com.rzm.commonlibrary.recyclerview.interfaces;
+
+import android.view.View;
+
+/**
+ * LongClick
+ */
+
+public interface OnItemLongClickListener {
+    void onItemLongClick(View view, int position);
+}
