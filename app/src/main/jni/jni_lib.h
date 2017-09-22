aa4697482691fccf741e6787ba101a4cf6d1cd72
renzhenming <1565766177@qq.com>
2017-09-22 22:45:41 +0800

    编译运行第一个jni函数

---
+++ app/src/main/jni/jni_lib.h
@@ -0,0 +1,10 @@
+//
+// Created by renzhenming on 2017/9/22.
+//
+
+#ifndef APPMARKETDEMO_JNI_LIB_H
+#define APPMARKETDEMO_JNI_LIB_H
+
+#include "jni.h"
+jstring GetStrFromJNI(JNIEnv* env,jobject callObj);
+#endif //APPMARKETDEMO_JNI_LIB_H
