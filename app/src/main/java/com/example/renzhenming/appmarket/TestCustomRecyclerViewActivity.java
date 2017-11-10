012224f3ad9e4028987845c284588a73830b1c9b
renzhenming <Renzhenming1991>
2017-11-10 18:50:51 +0800

    basically finish recyclerview function

--- app/src/main/java/com/example/renzhenming/appmarket/TestCustomRecyclerViewActivity.java
+++ app/src/main/java/com/example/renzhenming/appmarket/TestCustomRecyclerViewActivity.java
@@ -1,9 +1,13 @@
 package com.example.renzhenming.appmarket;
 
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,6 +20,7 @@ import com.example.mylibrary.view.indicator.recyclerview.view.LoadRefreshRecycle
 import com.example.mylibrary.view.indicator.recyclerview.view.RefreshRecyclerView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TestCustomRecyclerViewActivity extends AppCompatActivity {
@@ -58,6 +63,8 @@ public class TestCustomRecyclerViewActivity extends AppCompatActivity {
         mRecyclerView.addLoadViewCreator(new DefaultLoadCreator());
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         mRecyclerView.setAdapter(adapter);
+        // 这个就不多解释了，就这么attach
+        itemTouchHelper.attachToRecyclerView(mRecyclerView);
         mRecyclerView.setOnRefreshListener(new RefreshRecyclerView.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -123,4 +130,87 @@ public class TestCustomRecyclerViewActivity extends AppCompatActivity {
         },2000);
 
     }
+
+    // 实现左边侧滑删除 和 拖动排序
+    ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.Callback() {
+        @Override
+        public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            // 获取触摸响应的方向   包含两个 1.拖动dragFlags 2.侧滑删除swipeFlags
+            // 代表只能是向左侧滑删除，当前可以是这样ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT
+            int swipeFlags = ItemTouchHelper.LEFT;
+
+
+            // 拖动
+            int dragFlags = 0;
+            if (recyclerView.getLayoutManager() instanceof GridLayoutManager) {
+                // GridView 样式四个方向都可以
+                dragFlags = ItemTouchHelper.UP | ItemTouchHelper.LEFT |
+                        ItemTouchHelper.DOWN | ItemTouchHelper.RIGHT;
+            } else {
+                // ListView 样式不支持左右，只支持上下
+                dragFlags = ItemTouchHelper.UP |
+                        ItemTouchHelper.DOWN;
+            }
+
+            return makeMovementFlags(dragFlags, swipeFlags);
+        }
+
+        /**
+         * 拖动的时候不断的回调方法
+         */
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+            // 获取原来的位置
+            int fromPosition = viewHolder.getAdapterPosition();
+            // 得到目标的位置
+            int targetPosition = target.getAdapterPosition();
+            if (fromPosition > targetPosition) {
+                for (int i = fromPosition; i < targetPosition; i++) {
+                    Collections.swap(mList, i, i + 1);// 改变实际的数据集
+                }
+            } else {
+                for (int i = fromPosition; i > targetPosition; i--) {
+                    Collections.swap(mList, i, i - 1);// 改变实际的数据集
+                }
+            }
+            adapter.notifyItemMoved(fromPosition, targetPosition);
+            return true;
+        }
+
+        /**
+         * 侧滑删除后会回调的方法
+         */
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+            // 获取当前删除的位置
+            int position = viewHolder.getAdapterPosition();
+            mList.remove(position);
+            // adapter 更新notify当前位置删除
+            adapter.notifyItemRemoved(position);
+        }
+
+        /**
+         * 拖动选择状态改变回调
+         */
+        @Override
+        public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+            if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+                // ItemTouchHelper.ACTION_STATE_IDLE 看看源码解释就能理解了
+                // 侧滑或者拖动的时候背景设置为灰色
+                viewHolder.itemView.setBackgroundColor(Color.GRAY);
+            }
+        }
+
+        /**
+         * 回到正常状态的时候回调
+         */
+        @Override
+        public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            // 正常默认状态下背景恢复默认
+            viewHolder.itemView.setBackgroundColor(0);
+            viewHolder.itemView.setTranslationX(0);
+        }
+    });
+
+
 }
