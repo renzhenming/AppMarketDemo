b8ebfe91debe06db8ea285470ba74eb32fa24dca
renzhenming <Renzhenming1991>
2018-01-09 10:39:55 +0800

    toolbar relate

--- app/src/main/java/com/app/rzm/TestCoordinatorCollapsingToolbarActivity.java
+++ app/src/main/java/com/app/rzm/TestCoordinatorCollapsingToolbarActivity.java
@@ -2,14 +2,20 @@ package com.app.rzm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.app.rzm.R;
 
 public class TestCoordinatorCollapsingToolbarActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_coordinator_collapsing_toolbar);
+        /*mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("作品");*/
     }
 }
