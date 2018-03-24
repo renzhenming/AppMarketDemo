32202fc7332666daf1fcb2cbd7292beb429b03a6
renzhenming <1565766177@qq.com>
2018-03-24 19:14:14 +0800

    add alphabetView

---
+++ app/src/main/java/com/app/rzm/test/TestAlphabetViewActivity.java
@@ -0,0 +1,27 @@
+package com.app.rzm.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.app.rzm.R;
+import com.rzm.mylibrary.view.AlphabetView;
+
+public class TestAlphabetViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_alphabet_view);
+        AlphabetView alphabet = (AlphabetView) findViewById(R.id.alphabet);
+
+        //设置当前选中的一直处于高亮状态
+        alphabet.setClearAfterUp(false);
+        alphabet.setOnAlphabetTouchListener(new AlphabetView.OnAlphabetTouchListener() {
+            @Override
+            public void onTouch(String letter, boolean isTouch) {
+                Toast.makeText(getApplicationContext(),letter,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
